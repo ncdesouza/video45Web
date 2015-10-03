6cb05d2a77e87ab21ed91d7378b60562ea7fd757
ncdesouza <nicholas.desouza@uoit.net>
2015-10-03 10:37:25 -0400

    fixed new user hang bug

--- src/app/controllers/user/profile.js
+++ src/app/controllers/user/profile.js
@@ -9,28 +9,28 @@ module.exports = function(app, passport, isLoggedIn) {
     // Profile =================================================================
     // =========================================================================
     app.get('/profile', isLoggedIn, function(request, response) {
-        User
-            .findOne({ email: request.user.email })
-            .exec(function(err, user) {
-                if (err) return console.log(err);
-                if (user.videos.length != 0) {
+        console.log(request.user.videos);
+        if (request.user.videos.length !== 0) {
+
+            User
+                .findOne({ email: request.user.email })
+                .exec(function(err, user) {
+
+                    if (err) return console.log(err);
+
                     user.getVideos(function (err, videos) {
                         console.log(videos);
                         response.render('profile.jade', {
                             videos: JSON.stringify(videos) // get user from session
                         });
                     });
-                } else {
-                    response.render('profile.jade', {
-                        videos: null // get user from session
-                    });
-                }
+                });
+        } else {
+
+            response.render('profile.jade', {
+                videos: null // get user from session
             });
-
-
-
-
-
+        }
     });
 
 
