8b592d0db057bbcb4ef83ea59855dbbc0cd3887e
ncdesouza <nicholas.desouza@uoit.net>
2015-12-26 12:10:32 -0500

    test webhook

--- src/app/controllers/api/github.js
+++ src/app/controllers/api/github.js
@@ -10,6 +10,7 @@ module.exports = function(app, passport) {
         var payload = req.body;
         if (payload.repository.full_name == 'ncdesouza/video45Web') {
             if (payload.ref == 'refs/head/master') {
+                console.log(payload.ref);
                 console.log('Preforming a Git Pull');
                 exec('/home/video45/www/video45Web/script/github/hook.sh',
                 function(err, stdout, stderr) {
