0a76c87626694a2bb9a6605521ab7d3bde00ec99
Nicholas De Souza <nicholas@naturosites.com>
2016-01-09 12:52:38 -0500

    config server json post

--- src/app/controllers/api/login.js
+++ src/app/controllers/api/login.js
@@ -6,8 +6,8 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = function(app, passport) {
-    app.post('/api/login',
-        passport.authenticate('login'),
+
+    app.post('/api/login', passport.authenticate('login'),
         function(req, res) {
             var payload = {username: req.user.username};
             var token = jwt.sign(payload, app.get('superSecret'), {
@@ -24,35 +24,37 @@ module.exports = function(app, passport) {
 
 
     app.post('/react/login',
-        User.findOne({ 'email' : email } , function(err, user) {
+        function(req, res) {
+            User.findOne({ 'email' : email } , function(err, user) {
 
-            // if errors, return the errors
-            if (err) {
-                return res.status(500).send({
-                    success:false,
-                    message: 'Server Error',
-                    token: null
+                // if errors, return the errors
+                if (err) {
+                    return res.status(500).send({
+                        success:false,
+                        message: 'Server Error',
+                        token: null
+                    });
+                }
+
+                // if user not found or invalid password
+                if (!user || !user.validPassword(password)) {
+                    return res.status(401).json({
+                        success: false,
+                        message: 'Username or password is incorrect',
+                        token: null
+                    });
+                }
+
+                // success: create token and send to client
+                var payload = {username: req.user.username};
+                var token = jwt.sign(payload, app.get('superSecret'), {expiresIn: 86400});
+                return res.status(200).json({
+                    success: true,
+                    message: 'Welcome to video45',
+                    token: token
                 });
-            }
-
-            // if user not found or invalid password
-            if (!user || !user.validPassword(password)) {
-                return res.status(403).json({
-                    success: false,
-                    message: 'Username or password is incorrect',
-                    token: null
-                });
-            }
-
-            // success: create token and send to client
-            var payload = {username: req.user.username};
-            var token = jwt.sign(payload, app.get('superSecret'), {expiresIn: 86400});
-            return res.status(200).json({
-                success: true,
-                message: 'Welcome to video45',
-                token: token
-            });
-        })
+            })
+        }
     )
 
 };
