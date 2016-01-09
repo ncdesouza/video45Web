120cbccd10b05b99019884362ee16047c9a07c0a
Nicholas De Souza <nicholas@naturosites.com>
2016-01-09 12:48:19 -0500

    config server json post

--- src/app/controllers/api/login.js
+++ src/app/controllers/api/login.js
@@ -21,4 +21,38 @@ module.exports = function(app, passport) {
             });
         }
     );
+
+
+    app.post('/react/login',
+        User.findOne({ 'email' : email } , function(err, user) {
+
+            // if errors, return the errors
+            if (err) {
+                return res.status(500).send({
+                    success:false,
+                    message: 'Server Error',
+                    token: null
+                });
+            }
+
+            // if user not found or invalid password
+            if (!user || !user.validPassword(password)) {
+                return res.status(403).json({
+                    success: false,
+                    message: 'Username or password is incorrect',
+                    token: null
+                });
+            }
+
+            // success: create token and send to client
+            var payload = {username: req.user.username};
+            var token = jwt.sign(payload, app.get('superSecret'), {expiresIn: 86400});
+            return res.status(200).json({
+                success: true,
+                message: 'Welcome to video45',
+                token: token
+            });
+        })
+    )
+
 };
