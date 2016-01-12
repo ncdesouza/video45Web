f05c088ac29d1ca4c6d1208f3af599f8538d1801
Nicholas De Souza <nicholas@naturosites.com>
2016-01-11 21:42:35 -0500

    enable cross-domain requests

--- src/server.js
+++ src/server.js
@@ -52,6 +52,21 @@ app.use(upload);
 app.use('/public', express.static('../public'));
 app.use('/bower_components',  express.static('../bower_components'));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
 
 // controllers ======================================================================
 require('./app/controllers/public/public.js')(app, passport, transporter);
