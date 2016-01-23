bb8b71c9cb97ffd1b324b6f420e769d5c6e87ee7
Nicholas De Souza <nicholas@naturosites.com>
2016-01-23 04:44:04 -0500

    added: api for profile

--- src/app/controllers/api/profile.js
+++ src/app/controllers/api/profile.js
@@ -15,7 +15,9 @@ module.exports = function (app, isValidUser) {
     });
 
     app.get('/api/:username', isValidUser, function(req, res) {
+        console.log(req.params.username);
         User.findOne({username: req.params.username}, function(err, user) {
+            if (err) throw err;
             user.getVideos(function (err, user) {
                 for (var i = 0; i < user.videos.length; i++) {
                     user.videos[i].liked = user.videos[i].likes.indexOf(user._id) > -1;
