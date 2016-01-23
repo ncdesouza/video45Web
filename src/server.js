a484e352e579095986a6859cc9bb3ee4480ee044
Nicholas De Souza <nicholas@naturosites.com>
2016-01-23 17:16:07 -0500

    added: api for home

--- src/server.js
+++ src/server.js
@@ -54,6 +54,7 @@ app.use('/bower_components',  express.static('../bower_components'));
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
